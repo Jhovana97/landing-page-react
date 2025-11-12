95d002231d7f47322ea074267f208b01ed8e9800
Jhovana <jhovanapatty95@gmail.com>
2025-11-12 15:50:48 -0300

    api rest con express

--- backend-landing/src/server.js
+++ backend-landing/src/server.js
@@ -6,23 +6,29 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const db = mysql.createConnection({
+
+const db = mysql.createPool({
     host: "localhost",
     user: "root",
     password: "",
     database: "portafolio",
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-db.connect((err) => {
+db.getConnection((err, connection) => {
     if (err) {
         console.error("❌ Error al conectar con MySQL:", err);
     } else {
         console.log("✅ Conectado a MySQL correctamente.");
+        connection.release(); // Liberamos la conexión
     }
 });
 
+
 app.get("/", (req, res) => {
-    res.json({ message: "Servidor backend funcionando correctamente 🚀" });
+    res.json({ message: "Servidor backend funcionando correctamente " });
 });
 
 app.get("/api/proyectos", (req, res) => {
@@ -36,6 +42,24 @@ app.get("/api/proyectos", (req, res) => {
         }
     });
 });
+
+//ruta crear 
+app.post("/api/proyectos", (req, res)=> {
+    const { titulo, descripcion, detalles, imagen, link } = req.body;
+
+    //llevar los datos a la base de datos
+    const query = "INSERT INTO proyectos (titulo, descripcion, detalles, imagen, link) VALUES (?, ?, ?, ?,?)";
+    db.query(query, [titulo, descripcion, detalles, imagen, link], (err, results) => { {
+        if (err) {
+            console.error("❌ Error al crear proyecto:", err);      
+            res.status(500).json({ error: "Error al crear el proyecto" });
+        } else {
+            res.status(201).json({ message: "Proyecto creado correctamente", datos: req.body });
+        }       
+    }});
+}) 
+
+
 
 const PORT = 5000;
 app.listen(PORT, () =>
